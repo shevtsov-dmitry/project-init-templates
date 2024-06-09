4e125e3d1d07e48d8f974662c85402c943d0bf6d
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-06-09 11:36:48 +0300

    spring-web-devtools

---
+++ spring-web-devtools/src/test/java/ru/clean/CleanApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
