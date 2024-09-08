959fe828ed9a41365ad7181690c63ee3ce01078f
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-09-08 12:56:48 +0300

    update spring and react-tailwind templates

--- tailwind-react/src/App.jsx
+++ tailwind-react/src/App.jsx
@@ -1,7 +1,6 @@
-function App() {
-  return (
-    <div className='bg-red-700 text-white font-bold font-serif italic size-20'>Title</div>
-  )
+export default function App() {
+    return (
+        <div className='bg-red-700 text-white font-bold font-serif italic size-20'>Title</div>
+    )
 }
 
-export default App
