ee89636f69451441e715b3b1fc3edece88faede6
shevtsov-dmitry <shadowstakeus28@gmail.com>
2024-09-09 08:01:30 +0300

    kotlin jar init fixes

--- kotlin-jar/src/main/kotlin/Main.kt
+++ kotlin-jar/src/main/kotlin/Main.kt
@@ -1,12 +1,5 @@
-package org.script
-
-import com.google.gson.Gson
+package custom.script
 
 fun main() {
-    class Person(val name: String, val surname: String, val age: Int)
-    val p  = Person("John", "Pimple", 52)
-    val gson = Gson()
-    val json = gson.toJson(p)
-    print(json)
 }
 
