68baf5d6638b22b107b40a30eaa18e755e9144da
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-01-21 15:40:20 +0300

    react native + tailwind template

--- web-spring-maven/src/main/java/com/webspringmaven/WebSpringMavenApplication.java
+++ web-spring-maven/src/main/java/com/webspringmaven/WebSpringMavenApplication.java
@@ -8,7 +8,6 @@ public class WebSpringMavenApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebSpringMavenApplication.class, args);
-        StringBuilder sb = new StringBuilder();
     }
 
 }
