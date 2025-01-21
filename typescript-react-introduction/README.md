38f071e0ac7afb4af537cbad22209004a321b3e7
shevtsov-dmitry <shadowstakeus28@gmail.com>
2025-01-21 15:37:54 +0300

    add template for tailwind react typescript

---
+++ typescript-react-introduction/README.md
@@ -0,0 +1,3 @@
+# APP
+
+`TODO: write README.md`
---
+++ typescript-react-introduction/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+
+  return (
+    <div className={"w-dvw h-dvh bg-[#333]"}>
+        <h1 className="text-2xl font-bold text-white underline">Vite, Typescript and React scratch.</h1>
+    </div>
+  )
+}
